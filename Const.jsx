94d65676bf3278a95e58c5c7e9182639be26e085
juanlayerinnovador <161651815+juanlayerinnovador@users.noreply.github.com>
2025-07-04 14:02:19 -0500

    Add files via upload

---
+++ Const.jsx
@@ -0,0 +1,3 @@
+export const ALUMNOS = [{id:1, nombre:'Juan', edad:18, carrera: 'ing. de alimentos', promedio: 4.5 }, {
+    id:2, nombre:'Luna', edad:17, carrera: 'ing. de sistemas', promedio: 3 }, {
+    id:3, nombre:'Maria', edad:19, carrera: 'Gastronomia', promedio: 4.5 }]
